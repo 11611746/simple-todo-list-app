a1531967a3d5272072f004c3ab0f329acd73602f
11611746 <bishaldutta64@gmail.com>
2020-10-10 16:28:02 +0530

    Updates

---
+++ precache-manifest.bf3deb941167976cf52234d3c8d59f5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "386e426070414a8ccfd51f24154b9ce5",
+    "url": "/simple-todo-list-app/index.html"
+  },
+  {
+    "revision": "a3e16d32b49bf99955dd",
+    "url": "/simple-todo-list-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a3e16d32b49bf99955dd",
+    "url": "/simple-todo-list-app/static/js/2.524de59b.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/simple-todo-list-app/static/js/2.524de59b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c429907657428e5426b",
+    "url": "/simple-todo-list-app/static/js/main.3c422f47.chunk.js"
+  },
+  {
+    "revision": "1c39fafdd13a6dfd826c",
+    "url": "/simple-todo-list-app/static/js/runtime-main.4510ede0.js"
+  }
+]);
